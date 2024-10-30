d52c5c300c06193638f7afb7e732af15abc3a9a1
Sergei <Sergei.stolpovskikh@gmail.com>
2024-10-30 15:20:09 +0300

    fixed rendering bug, changed request address to get more data

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -9,8 +9,12 @@ interface PostItemProps {
 export const PostItem: FC<PostItemProps> = ({ post }) => {
 	return (
 		<List.Item>
+			<div>
+				<span>Альбом: {post.albumId}</span>
+				<span>№ {post.id}</span>
+			</div>
 			<h2>{post.title}</h2>
-			<p>{post.body}</p>
+			<img src={post.thumbnailUrl} alt='image' />
 			<Button type='primary'>Редактировать</Button>
 			<Button danger>Удалить</Button>
 		</List.Item>
